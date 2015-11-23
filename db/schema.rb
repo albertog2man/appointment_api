49423a9a0821a4905a8d27f56d48b0c440d3d370
Albert Galvez <albertomgalvez@gmail.com>
2015-11-23 04:25:14 -0500

    added all the dates from the csv file

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151122072134) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "appointments", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
