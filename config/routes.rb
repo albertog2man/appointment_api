604c4baf1003f7ba71032c4cd4bc472c89c10df0
Albert Galvez <albertomgalvez@gmail.com>
2015-11-23 02:42:14 -0500

    typo in the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'appointment#index'
+  root 'appointments#index'
   resources :appointments
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
